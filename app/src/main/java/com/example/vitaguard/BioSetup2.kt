e5dd24f57f236747c51762db6c49dc14e0d06240
stantontrueheart <91921390+stantontrueheart@users.noreply.github.com>
2024-07-09 21:49:11 -0400

    - Added files for bio setup - Corrected crashing error

---
+++ app/src/main/java/com/example/vitaguard/BioSetup2.kt
@@ -0,0 +1,42 @@
+package com.example.vitaguard
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BioSetup2 : AppCompatActivity() {
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_one_time_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val check1 = findViewById<Switch>(R.id.passwordSwitch)
+        val password = findViewById<EditText>(R.id.passwordField)
+        val btn = findViewById<Button>(R.id.confirmButton)
+
+        check1.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) password.visibility = View.VISIBLE
+            if (!isChecked) password.visibility = View.INVISIBLE
+        }
+
+        btn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
